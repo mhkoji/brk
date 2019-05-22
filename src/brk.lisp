3bd78b0b4e869663d4d5767bc8a5453618e005b6
mhkoji <mhkoji2+git@gmail.com>
2019-05-22 21:30:46 +0900

    Draw brick

--- src/brk.lisp
+++ src/brk.lisp
@@ -14,6 +14,12 @@
   ((r :initarg :r
       :reader ball-r)))
 
+(defclass brick ()
+  ((w :initarg :w
+      :reader brick-w)
+   (h :initarg :h
+      :reader brick-h)))
+
 ;;;
 
 (defstruct position x y)
@@ -21,16 +27,6 @@
 (defclass positionable ()
   ((position :initarg :position
              :accessor position)))
-
-(defun square (x) (* x x))
-
-(defun squared-distance (positionable1 positionable2)
-  (let ((pos1 (position positionable1))
-        (pos2 (position positionable2)))
-    (+ (square (- (position-x pos1)
-                  (position-x pos2)))
-       (square (- (position-y pos1)
-                  (position-y pos2))))))
 
 (defclass velocity ()
   ((x :initarg :vx
@@ -42,7 +38,23 @@
 
 (defclass world-ball (ball positionable velocity) ())
 
-(defstruct world width height paddle ball)
+(defclass world-brick (brick positionable) ())
+
+(defstruct world width height paddle ball walls bricks)
+
+(defun change-ball-direction-on-collision-against-wall (world wall)
+  (let ((ball (world-ball world)))
+    (let ((pos (position ball)))
+      (ecase wall
+        (:left
+         (when (< (position-x pos) 0)
+           (setf (velocity-x ball) (- (velocity-x ball)))))
+        (:right
+         (when (< (world-width world) (position-x pos))
+           (setf (velocity-x ball) (- (velocity-x ball)))))
+        (:top
+         (when (< (position-y pos) 0)
+           (setf (velocity-y ball) (- (velocity-y ball)))))))))
 
 (let ((unit 2))
   (defun move-paddle-left (world)
@@ -64,23 +76,16 @@
       (incf (position-x pos) (* unit (velocity-x positionable)))
       (incf (position-y pos) (* unit (velocity-y positionable))))))
 
-(defun change-ball-direction-on-collision-againt-wall (world wall)
-  (let ((ball (world-ball world)))
-    (let ((pos (position ball)))
-      (ecase wall
-        (:left
-         (when (< (position-x pos) 0)
-           (setf (velocity-x ball) (- (velocity-x ball)))))
-        (:right
-         (when (< (world-width world) (position-x pos))
-           (setf (velocity-x ball) (- (velocity-x ball)))))
-        (:top
-         (when (< (position-y pos) 0)
-           (setf (velocity-y ball) (- (velocity-y ball)))))))))
-
 (defun ball-out-of-world-p (world)
   (let ((pos (position (world-ball world))))
     (< (world-height world) (position-y pos))))
+
+
+(defun auto-update-world (world)
+  (dolist (wall (world-walls world))
+    (change-ball-direction-on-collision-against-wall world wall))
+  (let ((ball (world-ball world)))
+    (move-by-velocity ball)))
 
 
 (defgeneric draw (object surface))
@@ -101,19 +106,35 @@
                     (paddle-h paddle)
                     :surface surface
                     :color sdl:*white*)))
+
+(defmethod draw ((brick world-brick) surface)
+  (let ((pos (position brick)))
+    (sdl:draw-box-* (position-x pos)
+                    (position-y pos)
+                    (brick-w brick)
+                    (brick-h brick)
+                    :surface surface
+                    :color sdl:*white*)))
+
 (defun main ()
   (let ((world (make-world
                 :width 320
                 :height 480
                 :ball (make-instance 'world-ball
                        :r 5
-                       :vx (sin (/ pi 4))
-                       :vy (- (sin (/ pi 4)))
+                       :vx (* 3 (sin (/ pi 4)))
+                       :vy (* 3 (- (sin (/ pi 4))))
                        :position (make-position :x 10 :y 420))
                 :paddle (make-instance 'world-paddle
                          :w 30
                          :h 5
-                         :position (make-position :x 10 :y 440))))
+                         :position (make-position :x 10 :y 440))
+                :walls (list :left :right :top)
+                :bricks (list (make-instance 'world-brick
+                                             :w 30
+                                             :h 5
+                                             :position (make-position
+                                                        :x 10 :y 20)))))
         (playing-p t))
     (sdl:with-init ()
       (sdl:window (world-width world)
@@ -130,16 +151,14 @@
                 ((sdl:key-down-p :sdl-key-right)
                  (move-paddle-right world)))
           (cond (playing-p
-                 (dolist (wall '(:left :right :top))
-                   (change-ball-direction-on-collision-againt-wall
-                    world wall))
-                 (let ((ball (world-ball world)))
-                   (move-by-velocity ball))
+                 (auto-update-world world)
                  (when (ball-out-of-world-p world)
                    (setf playing-p nil))
                  (sdl:clear-display sdl:*black*)
                  (draw (world-ball world) sdl:*default-display*)
                  (draw (world-paddle world) sdl:*default-display*)
+                 (dolist (brick (world-bricks world))
+                   (draw brick sdl:*default-display*))
                  (sdl:update-display))
                 (t
                  (sdl:clear-display sdl:*black*)
